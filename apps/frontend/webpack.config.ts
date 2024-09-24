b7c04d2f481f27dc5acacc9300df46de9f4a15a4
Karan Gupta <admin@joborce.com>
2024-09-25 00:23:47 +0530

    fix config

--- apps/frontend/webpack.config.ts
+++ apps/frontend/webpack.config.ts
@@ -1,21 +1,12 @@
+import dotenv from 'dotenv';
 import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin';
 import { EditorFeature, NegatedEditorFeature } from 'monaco-editor/esm/metadata';
+import path from 'path';
+import TerserPlugin from 'terser-webpack-plugin';
 import * as webpack from 'webpack';
 
-// Define the Rule interface based on your actual configuration structure
-interface Rule {
-    test?: RegExp; // Adjust this according to your actual rule structure
-}
-
-// Type guard to check if the value is a Rule
-function isRule(value: unknown): value is Rule {
-    return typeof value === 'object' && value !== null && 'test' in value;
-}
-
-// Define a type that extends StatsOptions to include warningsFilter
-interface CustomStatsOptions extends webpack.StatsOptions {
-    warningsFilter?: RegExp[];
-}
+// Setting environment variables from .env file
+dotenv.config({ path: path.resolve(process.env['PWD'] || process.cwd(), '.env') });
 
 // List of languages to include, constrained by EditorLanguage type
 const languages: Array<
@@ -128,49 +119,39 @@ export default (config: webpack.Configuration) => {
         })
     );
 
-    // Find the index of the CSS rule using the defined Rule type and type guard
-    const cssRuleIdx: number | undefined = config?.module?.rules
-        ?.filter(isRule) // Filter out invalid values
-        .findIndex((rule) => rule.test?.toString().includes(':css'));
-
-    // Check if cssRuleIdx is valid and not -1
-    if (cssRuleIdx !== undefined && cssRuleIdx !== -1) {
-        // Ensure rules is defined before splicing
-        config.module?.rules?.splice(cssRuleIdx, 1);
-    }
-
     config?.module?.rules?.push(
         {
             test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-        },
-        {
-            test: /\.(woff|woff2|eot|ttf|otf)$/,
             use: [
+                'style-loader',
                 {
-                    loader: 'file-loader',
+                    loader: 'css-loader',
                     options: {
-                        name: '[name].[hash].[ext]',
-                        outputPath: 'assets/fonts/',
-                        publicPath: 'assets/fonts/'
+                        url: false
                     }
                 }
             ]
+        },
+        {
+            test: /\.ttf$/,
+            type: 'asset/resource'
         }
     );
 
-    // Suppress CommonJS warnings
-    config.stats = config.stats || {};
-
-    // Type assertion to extend the config.stats type
-    (config.stats as CustomStatsOptions).warningsFilter = [
-        /end value has mixed support/,
-        /CommonJS or AMD dependencies can cause optimization bailouts/
+    config.ignoreWarnings = [
+        {
+            message: /CommonJS or AMD dependencies can cause optimization bailouts/
+        }
     ];
 
-    config.optimization = {
-        minimize: true
-    };
+    if (process.env['APP_ENV'] === 'PROD') {
+        config.optimization = {
+            minimize: true,
+            minimizer: [new TerserPlugin({ parallel: true })],
+            sideEffects: true,
+            usedExports: true
+        };
+    }
 
     return config;
 };
