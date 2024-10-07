661a1e9a23d10122f42cd83989ea7244bbee8c9e
Karan Gupta <karangupta0005@gmail.com>
2024-10-07 15:02:03 +0530

    test frontend pipeline

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -4,6 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { CONFIG } from './config/config';
 import { AppModule } from './modules/app.module';
 
+// setting up production mode
 if (CONFIG.production) {
     enableProdMode();
 }
